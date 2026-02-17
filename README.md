f1cf9ca4a16c9102c74b73151b65335938ac5011
GioAban <gioaban403@gmail.com>
2026-02-17 09:42:33 +0800

    Updated

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 <!-- npm run build → regenerate the static files
-
 git add -f out && git commit -m "update site"
+git subtree push --prefix out origin gh-pages
 
-git subtree push --prefix out origin gh-pages -->
+
+npm run dev:all
+"Run the package.json"
+
+
+
+
+
+
+-->
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static HTML export
-  basePath: '/pangasinan-heritage', // replace with your GitHub repo name
+  output: "export", // enables static HTML export
+  basePath: "/pangasinan-heritage", // replace with your GitHub repo name
+  assetPrefix: "/pangasinan-heritage/",
   images: {
     unoptimized: true, // optional, prevents errors with next/image
   },
