503b3fc64027caf98b4eece7caf415717f929ae5
GioAban <gioaban403@gmail.com>
2026-02-22 00:19:41 +0800

    trailingSlash

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: "export",
   basePath: "/pangasinan-heritage",
-  trailingSlash: true,
   trailingSlash: true, // Importante ito para mahanap ng GitHub ang index.html sa bawat folders
   images: {
     unoptimized: true,
