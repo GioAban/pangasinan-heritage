9f89af5ed0a05207b8eeff9d1a8df7a783f92775
GioAban <gioaban403@gmail.com>
2026-02-21 23:56:46 +0800

    Deploying Sabog UI Hahaha

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // enables static HTML export
-  basePath: "/pangasinan-heritage", // replace with your GitHub repo name
-  assetPrefix: "/pangasinan-heritage/",
+  output: "export",
+  basePath: "/pangasinan-heritage",
+  // Alisin muna natin ang assetPrefix, Next.js na ang bahala rito dahil sa basePath
+  trailingSlash: true,
   images: {
-    unoptimized: true, // optional, prevents errors with next/image
+    unoptimized: true,
   },
 };
 
